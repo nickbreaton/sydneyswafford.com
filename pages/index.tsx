be69c5ae988fd0ce10247919acab96c2da210d28
nickbreaton <nick@breaton.com>
2020-03-13 23:13:37 -0400

    Gatsby to Next.js

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { GetStaticProps } from 'next'
+
+const Home: React.FC = () => {
+    return (
+        <div className="container">
+            <main>
+                <h1>sydneyswafford.com</h1>
+            </main>
+        </div>
+    )
+}
+
+export default Home
